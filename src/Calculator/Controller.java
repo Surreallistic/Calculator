afcc031ee28bd3c13a425c24a3b02abc8f32ca8f
Surreallistic <iluzion5@o2.pl>
2016-11-09 22:22:13 +0100

    init

---
+++ src/Calculator/Controller.java
@@ -0,0 +1,25 @@
+package Calculator;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField displayField;
+
+
+    public void handleDigitalNumber(ActionEvent actionEvent) {
+        System.out.println(getUsedSymbol(actionEvent));
+
+    }
+
+    public void handleCalcOperation(ActionEvent actionEvent) {
+        System.out.println(getUsedSymbol(actionEvent));
+
+    }
+
+    private String getUsedSymbol(ActionEvent event) {
+        return ((Button)event.getSource()).getText();
+    }
+}
