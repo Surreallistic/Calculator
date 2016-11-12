5cc3a966b006e2877590192a5a3a924529e41b58
Surreallistic <iluzion5@o2.pl>
2016-11-12 19:06:59 +0100

    css update

--- src/Calculator/Controller.java
+++ src/Calculator/Controller.java
@@ -3,19 +3,28 @@ package Calculator;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
     public TextField displayField;
+    public Button closeButton;
     private String prevValue;
     private String nextValue;
 
     public void handleDigitalNumber(ActionEvent actionEvent) {
         String digtNumb = getUsedSymbol(actionEvent);
+
+        displayField.setText(getUsedSymbol(actionEvent));
     }
 
     public void handleCalcOperation(ActionEvent actionEvent) {
         displayField.setText(getUsedSymbol(actionEvent));
+    }
+
+    public void handleCloseWindow(ActionEvent actionEvent) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 
     private String getUsedSymbol(ActionEvent event) {
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -5,19 +5,46 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
         primaryStage.setTitle("Simple Calculator");
         primaryStage.setScene(new Scene(root));
+        makeMovableWindow(root, primaryStage);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void makeMovableWindow(Parent root, Stage stage) {
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
     }
 }
