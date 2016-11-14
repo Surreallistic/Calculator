a29fc1038142896012970c3b830c0643ac2163a0
Surreallistic <iluzion5@o2.pl>
2016-11-14 06:56:37 +0100

    changes

--- src/Calculator/Controller.java
+++ src/Calculator/Controller.java
@@ -7,9 +7,9 @@ import javafx.stage.Stage;
 
 public class Controller {
 
-
     //        currentResult = currentResult.replaceAll("\\D+",""); // get number, delete any other sign
-    private boolean status = true;
+    private boolean statusClearResultField = true;
+    private boolean statusPerformCalcOperation = false;
 
     public Button closeButton;
 
@@ -19,6 +19,8 @@ public class Controller {
     private String bufferMemoryField;
     private String bufferResultField;
     private String tempValue;
+    private String lastUsedSymbol;
+    private String lastResultField;
 
     public void handleCloseWindow(ActionEvent actionEvent) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
@@ -26,22 +28,73 @@ public class Controller {
     }
 
     public void handleDigitalNumber(ActionEvent actionEvent) {
-        if(!status) { displayFieldResult.setText(""); status=true; } // reset result textField
-        tempValue = displayFieldResult.getText() + getUsedSymbol(actionEvent);
-        bufferResultField =  tempValue;
-        displayFieldResult.setText(bufferResultField);
+        if(!statusClearResultField) {
+            displayFieldResult.setText("");
+            statusClearResultField = true;
+            statusPerformCalcOperation = true;
+        } // reset result textField and perform action
+        if(getUsedSymbol(actionEvent).equals(Symbol.ONE)) {
+            displayFieldResult.setText("0");
+        } else {
+            tempValue = displayFieldResult.getText() + getUsedSymbol(actionEvent);
+            bufferResultField =  tempValue;
+            displayFieldResult.setText(bufferResultField);
+        }
+
     }
 
     public void handleCalcOperation(ActionEvent actionEvent) {
+        if(statusPerformCalcOperation) { prepareOperation(); }
+        lastResultField = bufferResultField;
         tempValue = bufferResultField + " " + getUsedSymbol(actionEvent) + " ";
         if(bufferMemoryField == null) { bufferMemoryField = tempValue; } else { bufferMemoryField += tempValue; }
-
+        lastUsedSymbol = getUsedSymbol(actionEvent);
         displayFieldMemory.setText(bufferMemoryField);
-        status = false;
+        statusClearResultField = false;
     }
 
     private String getUsedSymbol(ActionEvent event) {
         return ((Button)event.getSource()).getText();
     }
 
+    private void prepareOperation() {
+        bufferResultField = Integer.toString(performOperation());
+        displayFieldResult.setText(bufferResultField);
+    }
+
+    private int performOperation() {
+        int temp1 = Integer.parseInt(bufferResultField);
+        int temp2 = Integer.parseInt(lastResultField);
+        int result = 0;
+
+        switch (lastUsedSymbol) {
+            case Symbol.PLUS:
+                result = temp1 + temp2;
+                break;
+            case Symbol.MINUS:
+                result = temp1 - temp2;
+                break;
+            case Symbol.DIVIDE:
+                result = temp1 / temp2;
+                break;
+            case Symbol.MULTIPLY:
+                result = temp1 * temp2;
+                break;
+        }
+
+        return result;
+    }
+
+    public void handleEqualOperation(ActionEvent actionEvent) {
+        bufferResultField = Integer.toString(performOperation());
+        displayFieldResult.setText(bufferResultField);
+    }
+
+    public void handleClearCalc(ActionEvent actionEvent) {
+        displayFieldMemory.setText("");
+        displayFieldResult.setText("0");
+        bufferMemoryField = "";
+        bufferResultField = "";
+        lastResultField = "0";
+    }
 }
---
+++ src/Calculator/Symbol.java
@@ -0,0 +1,21 @@
+package Calculator;
+
+/**
+ * Created by Surreallistic on 13.11.2016.
+ */
+public interface Symbol {
+    String ZERO = "0";
+    String ONE = "1";
+    String TWO = "2";
+    String THREE = "3";
+    String FOUR = "4";
+    String FIVE = "5";
+    String SIX = "6";
+    String SEVEN = "7";
+    String EIGHT = "8";
+    String NINE = "9";
+    String PLUS = "+";
+    String MINUS = "-";
+    String DIVIDE = "/";
+    String MULTIPLY = "*";
+}
